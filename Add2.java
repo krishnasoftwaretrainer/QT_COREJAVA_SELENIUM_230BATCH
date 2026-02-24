b303d74d483dbe4dd281e96b436fc73ac07f814d
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2026-02-24 11:30:04 +0530

    NotePad Programs

---
+++ Add2.java
@@ -0,0 +1,18 @@
+class Addition2Num
+{
+public static void main(String args[])
+{
+  int value1=5;
+  int value2=10;
+  int sum;
+      sum=value1+value2;
+System.out.print("Addition of given Two Numbers:");
+System.out.println(sum);
+System.out.print("Program Executed Successfully");
+
+}
+}
+
+
+
+
