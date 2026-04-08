920f22059425cb810a4edb6eb67d845daf2a4def
QAKRISHNA <krishnasoftwaretrainer@gmail.com>
2026-04-08 11:58:47 +0530

    April 8th Selenium Scripts

---
+++ Selenium_MavenProject_230/src/test/java/pom/POM_Locaters.java
@@ -0,0 +1,40 @@
+package pom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class POM_Locaters 
+{
+	WebDriver driver;
+	
+	public POM_Locaters(WebDriver driver) 
+	{
+		this.driver=driver;
+	}
+
+	By username=By.id("user-name");
+	By password=By.id("password");
+	By loginBtn=By.id("login-button");
+	
+	public void enterUsername(String userName) 
+	{
+	 driver.findElement(username).sendKeys(userName);
+	}
+	
+	public void enterPassword(String passWord) 
+	{
+	 driver.findElement(password).sendKeys(passWord);
+	}
+	
+	public void clickLoginBtn() 
+	{
+	 driver.findElement(loginBtn).click();
+	}
+	
+	public void loginToApp(String userName, String passWord) 
+	{
+		enterUsername(userName);
+		enterPassword(passWord);
+		clickLoginBtn();
+	}
+}
